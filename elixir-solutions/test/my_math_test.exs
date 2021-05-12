ed76f956c67fade3103aa29517271dca05b8b50a
Vincent <thisoneis4business@gmail.com>
2021-05-12 15:25:06 +0700

    tree

---
+++ elixir-solutions/test/my_math_test.exs
@@ -0,0 +1,19 @@
+#
+#  my_math_test.exs
+#  elixir-solutions
+#
+#  Created by d-exclaimation on 14:44.
+#  Copyright © 2021 d-exclaimation. All rights reserved.
+#
+
+defmodule MyMathTest do
+  @moduledoc """
+    Test
+  """
+  use ExUnit.Case
+  doctest FPX
+
+  test "Continuous subarray max" do
+    assert MyMath.max_continous([34, -50, 42, 14, -5, 86]) == 137
+  end
+end
---
+++ elixir-solutions/test/tree_test.exs
@@ -0,0 +1,64 @@
+#
+#  tree_test.exs
+#  elixir-solutions
+#
+#  Created by d-exclaimation on 14:58.
+#  Copyright © 2021 d-exclaimation. All rights reserved.
+#
+
+defmodule FPX.TreeTest do
+  @moduledoc """
+    Tree Test
+  """
+  use ExUnit.Case
+  doctest FPX
+
+  test "Inverse a tree" do
+    init = %Tree{
+      val: 1,
+      left: %Tree{
+        val: 2,
+        left: %Tree{
+          val: 3
+        },
+        right: %Tree{
+          val: 4
+        }
+      },
+      right: %Tree{
+        val: 5,
+        left: %Tree{
+          val: 6
+        },
+        right: %Tree{
+          val: 7
+        }
+      }
+    }
+
+    expected = %Tree{
+      val: 1,
+      right: %Tree{
+        val: 2,
+        right: %Tree{
+          val: 3
+        },
+        left: %Tree{
+          val: 4
+        }
+      },
+      left: %Tree{
+        val: 5,
+        right: %Tree{
+          val: 6
+        },
+        left: %Tree{
+          val: 7
+        }
+      }
+    }
+
+    IO.inspect(init)
+    assert Kernel.inspect(init |> Tree.inversed()) == Kernel.inspect(expected)
+  end
+end
