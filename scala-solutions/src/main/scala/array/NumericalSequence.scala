86d87be35cef3cc8a8607d8e5a52db3b54779aa0
Vincent <thisoneis4business@gmail.com>
2021-07-27 22:49:21 +0700

    idk:

--- scala-solutions/src/main/scala/array/NumericalSequence.scala
+++ scala-solutions/src/main/scala/array/NumericalSequence.scala
@@ -12,15 +12,16 @@ import option.Nullable.optionToNullable
 import scala.language.implicitConversions
 
 class NumericalSequence(val wrapped: Iterable[Int]) {
-  /**
-   * Check whether numerical sequence is in increasing order
-   *
-   * '''Complexity''':
-   *
-   * Time: ''O(n)'' | Space: ''O(1)''
-   *
-   * @return Boolean whether it's increasing
-   */
+
+  /** Check whether numerical sequence is in increasing order
+    *
+    * '''Complexity''':
+    *
+    * Time: ''O(n)'' | Space: ''O(1)''
+    *
+    * @return
+    *   Boolean whether it's increasing
+    */
   def isIncreasing: Boolean = {
     val (_, res) = wrapped.foldLeft((0, false)) { (acc, x) =>
       val (prev, res) = acc
@@ -31,19 +32,21 @@ class NumericalSequence(val wrapped: Iterable[Int]) {
 }
 
 object NumericalSequence {
-  implicit def iterableToNumericalSequence(value: Iterable[Int]): NumericalSequence = new NumericalSequence(value)
+  implicit def iterableToNumericalSequence(
+      value: Iterable[Int]
+  ): NumericalSequence = new NumericalSequence(value)
 
-  /**
-   * Up, Up and Away
-   *
-   * Check if possible to change one value to make it all increasing order
-   *
-   * @param arr List of Integers.
-   * @return Boolean whether it is possible.
-   */
+  /** Up, Up and Away
+    *
+    * Check if possible to change one value to make it all increasing order
+    *
+    * @param arr
+    *   List of Integers.
+    * @return
+    *   Boolean whether it is possible.
+    */
   def upUpAndAway(arr: Vector[Int]): Boolean = {
-    arr
-      .indices
+    arr.indices
       .count(i =>
         (if (i > 0) arr(i - 1) else arr(i)) <= arr(i)
           && arr(i) <= (if (i < arr.length - 1) arr(i + 1) else arr(i))
@@ -51,15 +54,14 @@ object NumericalSequence {
   }
 
   def discountedPrice(vec: Vector[Int]): Vector[Int] = {
-    vec.indices.map(i => {
-      val discount = vec
-        .indices
-        .slice(i + 1, vec.length)
-        .find(j => vec(j) <= vec(i))
-        .?>(vec(_))
-      vec(i) - (discount ?? 0)
-    }).toVector
+    vec.indices
+      .map(i => {
+        val discount = vec.indices
+          .slice(i + 1, vec.length)
+          .find(j => vec(j) <= vec(i))
+          .?>(vec(_))
+        vec(i) - (discount ?? 0)
+      })
+      .toVector
   }
 }
-
-
--- scala-solutions/src/main/scala/pipe/Pipeable.scala
+++ scala-solutions/src/main/scala/pipe/Pipeable.scala
@@ -9,59 +9,72 @@ package pipe
 
 import scala.language.implicitConversions
 
-/**
- * Pipeable value which any value
- *
- * @param wrapped Any
- * @tparam T Any
- */
+/** Pipeable value which any value
+  *
+  * @param wrapped
+  *   Any
+  * @tparam T
+  *   Any
+  */
 class Pipeable[T](val wrapped: T) {
 
-  /**
-   * Pipe a function [Operator]
-   *
-   * ''* :: Piping function can do side effects, but not perform mutation''
-   *
-   * @param pipe Piping *pure function.
-   * @tparam K Transformation result type.
-   * @return A new value of type K.
-   */
+  /** Pipe a function [Operator]
+    *
+    * ''* :: Piping function can do side effects, but not perform mutation''
+    *
+    * @param pipe
+    *   Piping *pure function.
+    * @tparam K
+    *   Transformation result type.
+    * @return
+    *   A new value of type K.
+    */
   def |>[K](pipe: (T) => K): K = pipe(this.wrapped)
 
-  /**
-   * Pipe a function [Method]
-   *
-   * ''* :: Piping function can do side effects, but not perform mutation''
-   *
-   * @param pipe Piping *pure function.
-   * @tparam K Transformation result type.
-   * @return A new value of type K.
-   */
+  /** Pipe a function [Method]
+    *
+    * ''* :: Piping function can do side effects, but not perform mutation''
+    *
+    * @param pipe
+    *   Piping *pure function.
+    * @tparam K
+    *   Transformation result type.
+    * @return
+    *   A new value of type K.
+    */
   def pipe[K](pipe: (T) => K): K = pipe(this.wrapped)
 
+  /** Perform a tap on the value and return original value
+    *
+    * @param effect
+    *   Side effect perform (Must not mutate the data piped).
+    * @return
+    *   The initial value piped.
+    */
+  def tap(effect: (T) => Unit): T = {
+    effect(this.wrapped);
+    this.wrapped
+  }
 
-  /**
-   * Perform a tap on the value and return original value
-   * @param effect Side effect perform (Must not mutate the data piped).
-   * @return The initial value piped.
-   */
-  def tap(effect: (T) => Unit): T = { effect(this.wrapped); this.wrapped }
-
-  /**
-   * Checking whether this value can be piped
-   *
-   * @return boolean
-   */
+  /** Checking whether this value can be piped
+    *
+    * @return
+    *   boolean
+    */
   def isPipeable: Boolean = true
 }
 
 object Pipeable {
-  /**
-   * To make value pipeable
-   *
-   * @param s Value being transformed.
-   * @tparam T Type of the value.
-   * @return Pipeable[T]
-   */
+
+  /** To make value pipeable
+    *
+    * @param s
+    *   Value being transformed.
+    * @tparam T
+    *   Type of the value.
+    * @return
+    *   Pipeable[T]
+    */
   implicit def valueToPipeable[T](s: T): Pipeable[T] = new Pipeable[T](s)
+
 }
