5b4a6dd26a86b2cc38a8e5e02074663af410a204
Vincent <thisoneis4business@gmail.com>
2022-04-22 16:39:12 +0700

    idk

---
+++ swift-solutions/Sources/swift-solutions/State/Reducer.swift
@@ -0,0 +1,24 @@
+//
+//  Reducer.swift
+//  swift-solutions
+//
+//  Created by d-exclaimation on 16:07.
+//
+
+import Foundation
+
+struct Reducer<State: Sendable, Actions> {
+    private var reducer: (State, Actions) -> State
+    var state: State
+
+    init(_ reducer: @escaping (State, Actions) -> State, initial state: State) {
+        self.reducer = reducer
+        self.state = state
+    }
+    
+    mutating func dispatch(_ action: Actions) -> {
+        state = reducer(state, action)
+    }
+}
+
+
--- swift-solutions/Sources/swift-solutions/main.swift
+++ swift-solutions/Sources/swift-solutions/main.swift
@@ -7,4 +7,33 @@
 
 import Foundation
 
-print(uniqueCharacterSubstringCount(for: "aabca"))
+struct CounterState {
+    var count: Int
+}
+
+enum CounterAction {
+    case increment
+    case add(by: Int)
+    case decrement
+    case subtract(by: Int)
+    case empty
+}
+
+func counterReducer(_ state: CounterState, _ action: CounterAction) -> CounterState {
+    switch action {
+    case .increment:
+        return CounterState(count: state.count + 1)
+    case .add(let by):
+        return CounterState(count: state.count + by)
+    case .decrement:
+        return CounterState(count: state.count - 1)
+    case .subtract(let by):
+        return CounterState(count: state.count - by)
+    case .empty:
+        return CounterState(count: 0)
+    }
+}
+
+var reducer = Reducer(counterReducer, initial: CounterState(count: 0))
+
+
